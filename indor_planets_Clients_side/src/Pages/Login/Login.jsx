78bb3212af3ac765b26f242f308f279921362a05
Utso Roy <royutso003@gmail.com>
2025-07-28 11:50:43 +0600

    add strong password validation with uppercase, lowercase, number, and special character

--- indor_planets_Clients_side/src/Pages/Login/Login.jsx
+++ indor_planets_Clients_side/src/Pages/Login/Login.jsx
@@ -43,7 +43,13 @@ const Login = () => {
                   type="email"
                   className="input input-bordered w-full"
                   placeholder="Enter your email"
-                  {...register("email", { required: "Email is required" })}
+                  {...register("email", {
+                    required: "Email is required",
+                    pattern: {
+                      value: /^\S+@\S+$/i,
+                      message: "Invalid email format",
+                    },
+                  })}
                 />
                 <p className="text-red-500">{errors.email?.message}</p>
               </div>
@@ -56,16 +62,28 @@ const Login = () => {
                     type={showPassword ? "text" : "password"}
                     className="input input-bordered w-full pr-10"
                     placeholder="Enter your password"
-                    {...register("password", { required: "Password is required" })}
+                    {...register("password", {
+                        required: "Password is required",
+                          minLength: {
+                        value: 6,
+                        message: "Password must be at least 6 characters",
+                      },
+                      pattern: {
+                        value:
+                          /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[@$!%*?&]).{6,}$/,
+                        message:
+                          "Password must be at least 6 characters and include uppercase, lowercase, number, and special character.",
+                      },
+                    })}
                   />
-                
+
                   <span
                     className="absolute top-3 right-3 text-xl text-gray-500 cursor-pointer"
                     onClick={togglePassword}
                   >
                     {showPassword ? <AiFillEyeInvisible /> : <AiFillEye />}
-                                  </span>
-                                   <p className="text-red-500">{errors.password?.message}</p>
+                  </span>
+                  <p className="text-red-500">{errors.password?.message}</p>
                 </div>
               </div>
 
--- indor_planets_Clients_side/src/Pages/Register/Register.jsx
+++ indor_planets_Clients_side/src/Pages/Register/Register.jsx
@@ -11,7 +11,7 @@ const Register = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors }
+    formState: { errors },
   } = useForm();
 
   const onSubmit = (data) => {
@@ -22,10 +22,13 @@ const Register = () => {
     <div className="min-h-screen p-4 bg-base-300 flex justify-center items-center px-4">
       <div className="bg-white rounded-xl shadow-2xl w-full max-w-3xl p-6 lg:p-10">
         <div className="bg-gradient-to-t from-green-100 via-lime-100 to-green-50 flex flex-col lg:flex-row items-center overflow-hidden">
-          
           {/* Animation */}
           <div className="w-full lg:w-1/2 flex justify-center items-center p-4">
-            <Lottie animationData={animationData} loop={true} className="w-full max-w-sm h-auto" />
+            <Lottie
+              animationData={animationData}
+              loop={true}
+              className="w-full max-w-sm h-auto"
+            />
           </div>
 
           {/* Form */}
@@ -34,7 +37,6 @@ const Register = () => {
               Register
             </h2>
             <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-              
               {/* User Name */}
               <div>
                 <label className="label">User Name</label>
@@ -42,7 +44,9 @@ const Register = () => {
                   type="text"
                   className="input input-bordered w-full"
                   placeholder="Enter your user name"
-                  {...register("username", { required: "User Name is required" })}
+                  {...register("username", {
+                    required: "User Name is required",
+                  })}
                 />
                 <p className="text-red-500">{errors.username?.message}</p>
               </div>
@@ -54,7 +58,13 @@ const Register = () => {
                   type="email"
                   className="input input-bordered w-full"
                   placeholder="Enter your email"
-                  {...register("email", { required: "Email is required" })}
+                  {...register("email", {
+                    required: "Email is required",
+                    pattern: {
+                      value: /^\S+@\S+$/i,
+                      message: "Invalid email format",
+                    },
+                  })}
                 />
                 <p className="text-red-500">{errors.email?.message}</p>
               </div>
@@ -66,7 +76,9 @@ const Register = () => {
                   type="file"
                   accept="image/*"
                   className="file-input file-input-bordered w-full"
-                  {...register("profileImage", { required: "Image is required" })}
+                  {...register("profileImage", {
+                    required: "Image is required",
+                  })}
                 />
                 <p className="text-red-500">{errors.profileImage?.message}</p>
               </div>
@@ -79,7 +91,19 @@ const Register = () => {
                     type={showPassword ? "text" : "password"}
                     className="input input-bordered w-full pr-10"
                     placeholder="Enter your password"
-                    {...register("password", { required: "Password is required" })}
+                    {...register("password", {
+                      required: "Password is required",
+                      minLength: {
+                        value: 6,
+                        message: "Password must be at least 6 characters",
+                      },
+                      pattern: {
+                        value:
+                          /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[@$!%*?&]).{6,}$/,
+                        message:
+                          "Password must be at least 6 characters and include uppercase, lowercase, number, and special character.",
+                      },
+                    })}
                   />
                   <span
                     className="absolute top-3 right-3 text-xl text-gray-500 cursor-pointer"
@@ -92,12 +116,14 @@ const Register = () => {
               </div>
 
               {/* Submit */}
-              <button type="submit" className="btn bg-green-500 w-full text-white">
+              <button
+                type="submit"
+                className="btn bg-green-500 w-full text-white"
+              >
                 Register
               </button>
             </form>
           </div>
-
         </div>
       </div>
     </div>
