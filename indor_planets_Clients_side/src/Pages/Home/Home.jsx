1d770c561026fbe00ca437de3b024715656b4698
Utso Roy <royutso003@gmail.com>
2026-04-16 21:09:15 +0600

    home page image updated

--- indor_planets_Clients_side/src/Pages/Home/Home.jsx
+++ indor_planets_Clients_side/src/Pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import plantImg from "../../assets/Home section.png";
+import plantImg from "../../assets/plant_high_res.png";
 import { useNavigate } from "react-router";
 import Section1 from "../../Components/section1/Section1";
 import GradientText from "../../Components/GradientText/GradientText";
@@ -21,52 +21,49 @@ const Home = () => {
     AOS.init();
   }, []);
   return (
-  
-
-      <main className="">
+    <main className="">
       <div className=" bg-gradient-to-r from-green-100 via-lime-100 to-green-100   flex items-center justify-center px-6 py-1">
-          <Container>
-            
-                    <div className=" w-full grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
-          {/* Text Section */}
-          <div
-            data-aos="fade-right"
-            data-aos-easing="ease-out-cubic"
-            data-aos-duration="2000"
-          >
-            <p className="text-sm uppercase tracking-widest text-green-600">
-              Fresh • Natural • Green Living
-            </p>
-            <h1 className="text-4xl md:text-5xl font-bold text-green-800 mb-4">
-              Bring Nature Inside Your Home
-            </h1>
-            <p className="text-lg md:text-xl text-green-800 mb-6">
-              Discover our exclusive collection of air-purifying indoor plants
-              to refresh your space.
-            </p>
-            <button
-              onClick={() => navigate("/")}
-              className="bg-green-700 cursor-target hover:bg-green-800 cursor-pointer text-white px-6 py-3 rounded shadow-md transition-all duration-300"
+        <Container>
+          <div className=" w-full grid grid-cols-1 md:grid-cols-2 gap-10 h-screen items-center">
+            {/* Text Section */}
+            <div
+              data-aos="fade-right"
+              data-aos-easing="ease-out-cubic"
+              data-aos-duration="2000"
             >
-              Shop Now
-            </button>
-          </div>
+              <p className="text-sm uppercase tracking-widest text-green-600">
+                Fresh • Natural • Green Living
+              </p>
+              <h1 className="text-4xl md:text-5xl font-bold text-green-800 mb-4">
+                Bring Nature Inside Your Home
+              </h1>
+              <p className="text-lg md:text-xl text-green-800 mb-6">
+                Discover our exclusive collection of air-purifying indoor plants
+                to refresh your space.
+              </p>
+              <button
+                onClick={() => navigate("/")}
+                className="bg-green-700 cursor-target hover:bg-green-800 cursor-pointer text-white px-6 py-3 rounded shadow-md transition-all duration-300"
+              >
+                Shop Now
+              </button>
+            </div>
 
-          {/* Image Section */}
-          <div
-            data-aos="fade-down"
-            data-aos-easing="ease-out-cubic"
-            data-aos-duration="2000"
-            className="flex justify-center"
-          >
-            <img
-              src={plantImg}
-              alt="Indoor Plant"
-              className="max-w-[300px] md:max-w-[400px] w-full h-auto drop-shadow-lg"
-            />
+            {/* Image Section */}
+            <div
+              data-aos="fade-down"
+              data-aos-easing="ease-out-cubic"
+              data-aos-duration="2000"
+              className="flex justify-center"
+            >
+              <img
+                src={plantImg}
+                alt="Indoor Plant"
+                className="max-w-[300px] md:max-w-[400px] w-full h-auto drop-shadow-lg"
+              />
+            </div>
           </div>
-        </div>
-</Container>
+        </Container>
       </div>
       <section>
         <h2 className="text-center  text-[2rem] my-3">
@@ -79,7 +76,7 @@ const Home = () => {
             New Arrival Plants
           </GradientText>
         </h2>
-        <div >
+        <div>
           <NewArrivalSection></NewArrivalSection>
         </div>
       </section>
@@ -133,11 +130,8 @@ const Home = () => {
       </section>
       <section className="mt-5">
         <Contact></Contact>
-        
       </section>
     </main>
-
-    
   );
 };
 
